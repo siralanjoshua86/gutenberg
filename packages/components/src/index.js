5c1eb47f2d6fc0a990c6f5eede1850ceb0011339
Glen Davies <glendaviesnz@users.noreply.github.com>
2022-07-08 17:22:47 +1200

    Two steps forward - maybe?

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -31,6 +31,7 @@ export {
 } from './border-box-control';
 export { BorderControl as __experimentalBorderControl } from './border-control';
 export { default as __experimentalBoxControl } from './box-control';
+export { default as __experimentalBoxControlIcon } from './box-control/icon';
 export { default as Button } from './button';
 export { default as ButtonGroup } from './button-group';
 export {
