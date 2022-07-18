f8fa4d55768c2a7cbe44972afdcb23aa1bffdd4e
Marco Ciampini <marco.ciampo@gmail.com>
2022-07-18 15:41:36 +0200

    Rename middleware and placement variables

--- packages/components/src/popover/index.js
+++ packages/components/src/popover/index.js
@@ -104,7 +104,7 @@ const Popover = (
 		noArrow = true,
 		isAlternate,
 		position,
-		placement = 'bottom-start',
+		placement: placementProp = 'bottom-start',
 		offset,
 		focusOnMount = 'firstElement',
 		anchorRef,
@@ -132,9 +132,10 @@ const Popover = (
 	const isMobileViewport = useViewportMatch( 'medium', '<' );
 	const isExpanded = expandOnMobile && isMobileViewport;
 	const hasArrow = ! isExpanded && ! noArrow;
-	const usedPlacement = position
+
+	const normalizedPlacementProp = position
 		? positionToPlacement( position )
-		: placement;
+		: placementProp;
 
 	const ownerDocument = useMemo( () => {
 		if ( anchorRef?.top ) {
@@ -173,7 +174,7 @@ const Popover = (
 		return { x: iframeRect.left, y: iframeRect.top };
 	}, [ ownerDocument ] );
 
-	const middlewares = [
+	const middleware = [
 		frameOffset || offset
 			? offsetMiddleware( ( { placement: currentPlacement } ) => {
 					if ( ! frameOffset ) {
@@ -260,18 +261,15 @@ const Popover = (
 		strategy,
 		refs,
 		update,
-		placement: placementData,
+		placement: computedPlacement,
 		middlewareData: { arrow: arrowData = {} },
-	} = useFloating( {
-		placement: usedPlacement,
-		middleware: middlewares,
-	} );
+	} = useFloating( { placement: normalizedPlacementProp, middleware } );
 	const staticSide = {
 		top: 'bottom',
 		right: 'left',
 		bottom: 'top',
 		left: 'right',
-	}[ placementData.split( '-' )[ 0 ] ];
+	}[ computedPlacement.split( '-' )[ 0 ] ];
 	const mergedRefs = useMergeRefs( [ floating, dialogRef, ref ] );
 
 	// Updates references
@@ -358,7 +356,7 @@ const Popover = (
 		!! animate &&
 		getAnimateClassName( {
 			type: 'appear',
-			origin: placementToAnimationOrigin( placementData ),
+			origin: placementToAnimationOrigin( computedPlacement ),
 		} );
 
 	// Disable reason: We care to capture the _bubbled_ events from inputs
